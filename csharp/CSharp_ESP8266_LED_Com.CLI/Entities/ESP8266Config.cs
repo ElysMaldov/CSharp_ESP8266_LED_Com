9ce8103378d8b249b41d6b35cfc28929689f5d58
Elys Maldov <maldovelys@gmail.com>
2026-05-16 19:11:45 +0700

    feat: setup ESP8266Config

---
+++ csharp/CSharp_ESP8266_LED_Com.CLI/Entities/ESP8266Config.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Configuration;
+
+namespace CSharp_ESP8266_LED_Com.CLI.Entities;
+
+public record class ESP8266Config
+{
+
+
+  public int BaudRate { get; set; }
+  public int COMPort { get; set; }
+}
--- csharp/CSharp_ESP8266_LED_Com.CLI/Program.cs
+++ csharp/CSharp_ESP8266_LED_Com.CLI/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO.Ports;
+using CSharp_ESP8266_LED_Com.CLI.Entities;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+  public static void Main(string[] args)
+  {
+    // Setup appsettings
+    var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    IConfiguration configuration = builder.Build();
+
+    var esp8266Config = configuration.GetSection("ESP8266Config").Get<ESP8266Config>()
+      ?? throw new InvalidOperationException("ESP8266Config section is not initialized correctly."); ;
+
+    LEDController ledController = new(esp8266Config.COMPort, esp8266Config.BaudRate);
+  }
+}
